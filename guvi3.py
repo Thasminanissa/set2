b2eb2dfe1d91f6be2281509361b1347ae9e2afe6
Thasminanissa <42801326+Thasminanissa@users.noreply.github.com>
2018-09-24 15:16:59 +0530

    Create guvi3.py

---
+++ guvi3.py
@@ -0,0 +1,8 @@
+ch=raw_input()
+if((ch>="a" and ch<="z" )or (ch>="A" and ch<="Z")):
+	if((ch=="a" or ch=="e" or ch=="i" or ch=="o" or ch=="u") and (ch=="A" or ch=="E" or ch=="I" or ch=="O" or ch=="U")):
+		print("vowel")
+	else:
+		print("consonant")
+else:
+	print("invalid")
